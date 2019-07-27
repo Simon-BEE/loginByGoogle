687e329885af99ef9402eac6c17386ae1ea7b32f
simon-bee <simonbee1303@gmail.com>
2019-07-27 10:13:54 +0200

    connexion par google ok

---
+++ www/html/index.php
@@ -0,0 +1,59 @@
+<?php
+
+$basePath = dirname(__dir__) . DIRECTORY_SEPARATOR;
+require_once $basePath . 'vendor/autoload.php';
+
+require_once "config.php";
+
+$url = "http://localhost:8090";
+session_start();
+
+// algoritme pour la connexion via google
+//$_SESSION["user"] si connecter 
+
+$g_client = new Google_Client();
+$g_client->setClientId($id);
+$g_client->setClientSecret($secret);
+$g_client->setRedirectUri($url);
+$g_client->setScopes("email");
+
+$connectUrl = $g_client->createAuthUrl();
+
+$code = isset($_GET['code']) ? $_GET['code'] : NULL;
+
+if (isset($code)){
+    try{
+        $token = $g_client->fetchAccessTokenWithAuthCode($code);
+        $g_client->setAccessToken($token);
+    }catch(Exception $e){
+        echo $e->getMessage();
+    }
+
+    try{
+        $_SESSION["user"] = $g_client->verifyIdToken();
+    }catch(Exception $e){
+        echo $e->getMessage();
+    }
+
+}else{
+    $_SESSION["user"] = NULL;
+}
+
+if (isset($_SESSION["user"])) {
+    header('Location: ' . $url . "/protected.php");
+}
+
+include 'header.php';
+?>
+
+
+
+<div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+    <div class="col-md-6 px-0">
+        <h1 class="display-4 font-italic">Protected Zone</h1>
+        <p class="lead my-3"><a href='<?= $connectUrl ?>' class="btn btn-primary">Login Through Google </a></p>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
